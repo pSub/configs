1eefb6fb9e6a83d23c41ab60f039d401be442002
Pascal Wittmann <mail@pascal-wittmann.de>
2012-02-16 14:15:51 +0100

    updated orgmode config

--- .emacs.d/my-org-mode.el
+++ .emacs.d/my-org-mode.el
@@ -1,3 +1,7 @@
-(setq org-startup-indented t)
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
 
-(provide 'org-mode)
+(provide 'my-org-mode)
