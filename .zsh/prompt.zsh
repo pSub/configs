bd4ecdc88c5ca57619adccd85a426f03e3af8bd8
Pascal Wittmann <mail@pascal-wittmann.de>
2012-03-10 17:25:25 +0100

    zsh: display hostname in magenta on a remote host

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -24,6 +24,9 @@ function prompt_char() {
 # Mark username red if you are root
 if [ $UID -eq 0 ]; then usercolor="red"; else usercolor="cyan"; fi
 
+# Mark hostname magenta if you are on a remote host
+if [[ $(who am i) =~ (.+)$ ]]; then hostcolor="magenta"; else hostcolor="green"; fi
+
 # Prompt inspired by Steven Loshs http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-export PROMPT='%{$fg[$usercolor]%}%n%{$reset_color%} at %{$fg[green]%}%m%{$reset_color%} in %{$fg_bold[green]%}%c%{$reset_color%} $(prompt_char) '
+export PROMPT='%{$fg[$usercolor]%}%n%{$reset_color%} at %{$fg[$hostcolor]%}%m%{$reset_color%} in %{$fg_bold[green]%}%c%{$reset_color%} $(prompt_char) '
 export RPROMPT='${vcs_info_msg_0_} ${vcs_info_msg_2_} ${vcs_info_msg_3_}'
