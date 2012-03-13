621a6251273c8cc376c32eea68a86334f6503867
Pascal Wittmann <mail@pascal-wittmann.de>
2012-03-13 16:40:37 +0100

    some basic orge-mode configuration

--- .emacs.d/my-org-mode.el
+++ .emacs.d/my-org-mode.el
@@ -1,7 +1,42 @@
 (require 'org-install)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+(require 'org-contacts)
 
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-font-lock-mode 1)
+
+(setq org-directory "~/org/")
+
+(setq org-agenda-files (list (concat org-directory "agenda/"))
+      org-default-notes-file (concat org-directory "notes.org")
+      org-contacts-files (concat org-directory "contacts.org"))
+
+(setq org-refile-targets '((buffer-file-name :maxlevel . 3)
+                           (org-agenda-files :maxlevel . 2)
+                           )
+      org-refile-use-outline-path 'file)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file (format "%s/agenda/todo.org" org-directory))
+         "* TODO %?\n  %i\n  %a")
+        ("m" "Meeting" entry (file (format "%s/agenda/meetings.org" org-directory))
+         "* %?\n %i\n  %a")
+        ("x" "Note with Clipboard" entry (file (format "%s/notes.org" org-directory))
+         "* %?\n  %i\n  %x")
+        ("n" "Note" entry (file (format "%s/notes.org" org-directory))
+         "* %?\n  %i\n  %a")
+        ;; dedicated templates
+        ("s" "Save link for reading" entry (file+headline
+                                            (format "%s/links.org" org-directory)
+                                            "Unsorted")
+         "* %:description\n  %:link\n  %U"
+         )
+        ("c" "Contacts" entry (file (concat org-directory "contacts.org"))
+         "* %(org-contacts-template-name)
+:PROPERTIES:
+:EMAIL: %(org-contacts-template-email)
+:END:")
+        ))
 (provide 'my-org-mode)
