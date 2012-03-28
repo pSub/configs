7f37a1836b7e4981d7f950cc3e3eb69e857004eb
Pascal Wittmann <mail@pascal-wittmann.de>
2012-03-29 00:06:52 +0200

    [zsh] bound 'cd -<TAB>' to C-x C-f

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -55,13 +55,21 @@ global-alias-dot() {
    fi
 }
 
+# Handy use of dirstack bound to C-x C-f
+global-alias-dirstack() {
+   LBUFFER+="cd -"
+   zle expand-or-complete
+}
+
 # Register these functions as zle-widgets
 zle -N global-alias-space
 zle -N global-alias-tilde
 zle -N global-alias-dot
+zle -N global-alias-dirstack
 
 # Bind the appropriated keys to the expansion
 # widgets above.
 bindkey ' ' global-alias-space
 bindkey '~' global-alias-tilde
 bindkey '.' global-alias-dot
+bindkey '^X^f' global-alias-dirstack
