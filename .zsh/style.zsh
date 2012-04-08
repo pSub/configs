e30bc0fd6bd075d733b50254ea3f4407f3d8604c
Pascal Wittmann <mail@pascal-wittmann.de>
2012-04-08 23:18:37 +0200

    [zsh] fixed color leaking issue, that occured sometings on completion

--- .zsh/style.zsh
+++ .zsh/style.zsh
@@ -4,8 +4,8 @@
 zstyle ':vcs_info:*' enable git hg darcs cvs svn
 zstyle ':vcs_info:*' max-exports "4"
 zstyle ':vcs_info:*:prompt:*' check-for-changes true
-zstyle ':vcs_info:*:prompt:*' unstagedstr "%F{11}★%{$reset_color%}"
-zstyle ':vcs_info:*:prompt:*' stagedstr "%F{28}★%{$reset_color%}"
+zstyle ':vcs_info:*:prompt:*' unstagedstr "%{$fg[yellow]%}★%{$reset_color%}"
+zstyle ':vcs_info:*:prompt:*' stagedstr "%{$fg[green]%}★%{$reset_color%}"
 zstyle ':vcs_info:*:prompt:*' actionformats '%a'
 zstyle ':vcs_info:*:prompt:*' formats '%u%c' '%s' "on %{$fg[red]%}%b%{$reset_color%}" 'in %S'
 
