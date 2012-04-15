1a638d2ab464de2d6fe17c41917bb4f6b78846f5
Pascal Wittmann <mail@pascal-wittmann.de>
2012-04-15 13:27:47 +0200

    [zsh] removed obsolet chpwd function

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -41,6 +41,3 @@ if [[ -f $DIRSTACKFILE ]] && [[ $#dirstack -eq 0 ]]; then
   dirstack=( ${(f)"$(< $DIRSTACKFILE)"} )
   [[ -d $dirstack[1] ]] && cd $dirstack[1] && cd $OLDPWD
 fi
-chpwd() {
-  print -l $PWD ${(u)dirstack} >$DIRSTACKFILE
-}
