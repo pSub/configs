f699fdac1e0d1841307d6fd13bd88a7510d10f27
Pascal Wittmann <mail@pascal-wittmann.de>
2012-04-15 22:12:11 +0200

    Revert "[zsh] removed obsolet chpwd function"
    This reverts commit 1a638d2ab464de2d6fe17c41917bb4f6b78846f5.
    
    Don't know why I removed that function. Its necessary to save
    the directory on every change to DIRSTACKFILE, which the prehook
    chpwd.

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -8,3 +8,6 @@ if [[ -f $DIRSTACKFILE ]] && [[ $#dirstack -eq 0 ]]; then
   dirstack=( ${(f)"$(< $DIRSTACKFILE)"} )
   [[ -d $dirstack[1] ]] && cd $dirstack[1] && cd $OLDPWD
 fi
+chpwd() {
+  print -l $PWD ${(u)dirstack} >$DIRSTACKFILE
+}
