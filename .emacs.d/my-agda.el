59fc7bd01f52e61c0f36cd87f6e4aab99e3c549b
Pascal Wittmann <mail@pascal-wittmann.de>
2012-08-27 17:27:18 +0200

    [agda-mode] added library support

--- .emacs.d/my-agda.el
+++ .emacs.d/my-agda.el
@@ -1,5 +1,8 @@
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
-
+(custom-set-variables
+ '(agda2-include-dirs (quote ("." "~/.agda/src")))
+ '(haskell-notify-p t)
+ '(haskell-process-type (quote ghci)))
 
 (provide 'my-agda)
