9975f0acefd0aeed3465246ab3acefcd9c343e77
Pascal Wittmann <mail@pascal-wittmann.de>
2012-09-11 23:10:34 +0200

    [org-mode] many options and org-habit

--- .emacs.d/my-org-mode.el
+++ .emacs.d/my-org-mode.el
@@ -1,14 +1,34 @@
 (require 'org-install)
 (require 'org-contacts)
+(require 'org-habit)
 (require 'diary-lib)
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
 (global-font-lock-mode 1)
 
+;; Resume clocking tasks when emacs is restarted
+(org-clock-persistence-insinuate)
+
 (setq org-directory "~/org/")
+
+(setq org-hide-leading-stars 'hidestars
+      org-completion-use-ido t
+      org-log-done 'time
+
+      ;; Save clock data and notes in a separate drawer
+      org-clock-into-drawer "CLOCK"
+      org-clock-history-length 35
+      org-clock-in-resume t
+      org-clock-persist t
+      org-agenda-start-with-clockreport-mode t
+      org-agenda-window-setup 'current-window
+      org-agenda-clockreport-parameter-plist (quote (:link t :maxlevel 5 :fileskip0 t :compact t))
+      org-startup-indented t
+      )
 
 (setq org-agenda-files (list (concat org-directory "agenda/"))
       org-default-notes-file (concat org-directory "notes.org")
@@ -18,6 +38,25 @@
                            (org-agenda-files :maxlevel . 2)
                            )
       org-refile-use-outline-path 'file)
+
+(add-hook 'org-agenda-mode-hook '(lambda () (hl-line-mode 1 )))
+
+; Erase all reminders and rebuilt reminders for today from the agenda
+(defun my/org-agenda-to-appt ()
+  (interactive)
+  (setq appt-time-msg-list nil)
+  (org-agenda-to-appt))
+
+; Rebuild the reminders everytime the agenda is displayed
+(add-hook 'org-finalize-agenda-hook 'my/org-agenda-to-appt 'append)
+
+(my/org-agenda-to-appt)
+
+; Activate appointments so we get notifications
+(appt-activate t)
+
+; If we leave Emacs running overnight - reset the appointments one minute after midnight
+(run-at-time "24:01" nil 'my/org-agenda-to-appt)
 
 (setq org-capture-templates
       '(("t" "Todo" entry (file (format "%s/agenda/todo.org" org-directory))
