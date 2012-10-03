7e74a928a3a2713e6db659ea4d70b4e0b6187865
Pascal Wittmann <mail@pascal-wittmann.de>
2012-10-03 21:09:12 +0200

    [sync-unison] unified unison functions and put the profiles that shall be synchronized in an array (making all.prf obsolet)

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -6,10 +6,6 @@ alias ls="ls --color=auto"
 alias xfiglatex="xfig -specialtext -latexfonts -startlatexFont default"
 alias unpack="aunpack"
 alias pack="apack"
-
-# $HOME is the local root of unison.
-alias sync-unison="unison unison -nodeletion $HOME && unison all && unison unison"
-
 alias sul="su --login"
 alias ssh="TERM=xterm ssh"
 
