430bfb4643a12271d0a65946074285094ec7c56c
Pascal Wittmann <mail@pascal-wittmann.de>
2012-12-29 12:49:14 +0100

    [emacs/agda] updated library path

--- .emacs.d/my-agda.el
+++ .emacs.d/my-agda.el
@@ -1,8 +1,7 @@
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
 (custom-set-variables
- '(agda2-include-dirs (quote ("." "~/.agda/src")))
- '(haskell-notify-p t)
- '(haskell-process-type (quote ghci)))
+ '(agda2-include-dirs (quote ("." "/home/pascal/.agda/lib-0.6/src")))
+ )
 
 (provide 'my-agda)
