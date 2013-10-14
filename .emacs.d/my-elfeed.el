328e9361b170df5b73a78445a5b5c9645bccc581
Pascal Wittmann <mail@pascal-wittmann.de>
2013-10-14 12:56:15 +0200

    [elfeed] reload elfeed config before fetching new feeds

--- .emacs.d/my-elfeed.el
+++ .emacs.d/my-elfeed.el
@@ -1,6 +1,13 @@
 (require-package 'elfeed)
 (require 'elfeed)
 
+(defun elfeed-my-update ()
+  (interactive)
+  (load-file "~/.emacs.d/my-elfeed.el")
+  (elfeed-update))
+
+(define-key elfeed-search-mode-map "G" 'elfeed-my-update)
+
 (global-set-key (kbd "C-x w") 'elfeed)
 
 (setq elfeed-feeds (read-lines "~/.feeds"))
