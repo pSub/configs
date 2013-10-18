493d26e693f3596fae20daf2138eb231e8bc19c1
Pascal Wittmann <mail@pascal-wittmann.de>
2013-10-18 10:57:30 +0200

    [x220] removed environment for dwb

--- x220/nixpkgs/config.nix
+++ x220/nixpkgs/config.nix
@@ -4,11 +4,6 @@
   dwm.patches = [ ./dwm.patch ];
 
   packageOverrides = pkgs : with pkgs; {
-    dwbEnv = pkgs.myEnvFun {
-      name = "dwb";
-      buildInputs = [ stdenv libsoup webkit pkgconfig gnome3.gtk gnutls json_c m4 ];
-    };
-
     # How can I give that environment a name?
     # Should this be turned into a real environment?
     haskell_env = pkgs.haskellPackages.ghcWithPackages (pkgs : [
