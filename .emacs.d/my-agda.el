469581bee5da8fb4bee8230481121a588138ee1e
Pascal Wittmann <mail@pascal-wittmann.de>
2013-10-19 15:55:43 +0200

    [agda-mode] use a more recent standard library

--- .emacs.d/my-agda.el
+++ .emacs.d/my-agda.el
@@ -1,7 +1,7 @@
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
 (custom-set-variables
- '(agda2-include-dirs (quote ("." "/home/pascal/.agda/lib-0.6/src")))
+ '(agda2-include-dirs (quote ("." "/home/pascal/.agda/lib-0.7/src")))
  )
 
 (provide 'my-agda)
