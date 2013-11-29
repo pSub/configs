a87ab89a3e153c1600ab8362a262d66adcd313a4
Pascal Wittmann <mail@pascal-wittmann.de>
2013-11-29 15:00:48 +0100

    [zsh/prompt] Make the shell alerting.
    Not sure whether this is annoying, only time will tell.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -30,5 +30,5 @@ if [ $UID -eq 0 ]; then usercolor="red"; else usercolor="cyan"; fi
 if [[ $(who am i) =~ (.+)$ ]]; then hostcolor="magenta"; else hostcolor="green"; fi
 
 # Prompt inspired by Steven Loshs http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-export PROMPT='%{$fg[$usercolor]%}%n%{$reset_color%} at %{$fg[$hostcolor]%}%m%{$reset_color%} in %{$fg_bold[green]%}%c%{$reset_color%} $(prompt_char) '
+export PROMPT='%{$(echo "\a")%}%{$fg[$usercolor]%}%n%{$reset_color%} at %{$fg[$hostcolor]%}%m%{$reset_color%} in %{$fg_bold[green]%}%c%{$reset_color%} $(prompt_char) '
 export RPROMPT='${vcs_info_msg_0_} ${vcs_info_msg_2_} ${vcs_info_msg_3_}'
