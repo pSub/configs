1b9560ae8c94e22c8c10faacd8f8631155ce5cb2
Pascal Wittmann <mail@pascal-wittmann.de>
2013-12-04 17:21:27 +0100

    [org-mode] added fill-paragraph function

--- .emacs.d/my-auctex.el
+++ .emacs.d/my-auctex.el
@@ -1,3 +1,22 @@
+;; http://pleasefindattached.blogspot.de/2011/12/emacsauctex-sentence-fill-greatly.html
+(defadvice LaTeX-fill-region-as-paragraph (around LaTeX-sentence-filling)
+  "Start each sentence on a new line."
+  (let ((from (ad-get-arg 0))
+        (to-marker (set-marker (make-marker) (ad-get-arg 1)))
+        tmp-end)
+    (while (< from (marker-position to-marker))
+      (forward-sentence)
+      ;; might have gone beyond to-marker --- use whichever is smaller:
+      (ad-set-arg 1 (setq tmp-end (min (point) (marker-position to-marker))))
+      ad-do-it
+      (ad-set-arg 0 (setq from (point)))
+      (unless (or
+               (bolp)
+               (looking-at "\\s *$"))
+        (LaTeX-newline)))
+    (set-marker to-marker nil)))
+(ad-activate 'LaTeX-fill-region-as-paragraph)
+
 ;; Set PDF mode as default
 (setq TeX-PDF-mode t)
 
