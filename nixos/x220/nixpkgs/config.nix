7e2455e8cee7df2629ada1f530790b448d2229b3
Pascal Wittmann <mail@pascal-wittmann.de>
2014-01-12 20:56:34 +0100

    [x220] removed unused environment

--- nixos/x220/nixpkgs/config.nix
+++ nixos/x220/nixpkgs/config.nix
@@ -1,17 +1,6 @@
 
 {
 
-  packageOverrides = pkgs : with pkgs; {
-    # How can I give that environment a name?
-    # Should this be turned into a real environment?
-    haskell_env = pkgs.haskellPackages.ghcWithPackages (pkgs : [
-         pkgs.haskellPlatform
-         stdenv
-         zlib
-         haskellPackages.cabalDev
-         haskellPackages.hlint
-     ]);
-
    myTexLive = pkgs.texLiveAggregationFun {
      paths = [
        texLive texLiveExtra texLiveBeamer texLiveCMSuper
