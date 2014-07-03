386fb728037fbadc7eb3b229360497b97a7cfa37
Pascal Wittmann <mail@pascal-wittmann.de>
2014-07-04 00:13:59 +0200

    [nixos/server] upgrade kernel

--- nixos/server/configuration.nix
+++ nixos/server/configuration.nix
@@ -18,7 +18,7 @@ in {
       ./password.nix
     ];
 
-  boot.kernelPackages = pkgs.linuxPackages_3_10;
+  boot.kernelPackages = pkgs.linuxPackages_3_15;
 
   boot.initrd.kernelModules =
     [ # Specify all kernel modules that are necessary for mounting the root
