7a687205c40f88993ac70978ac185b26cb0a1db7
Pascal Wittmann <mail@pascal-wittmann.de>
2017-10-21 16:49:58 +0200

    [nixops/postgres] make localhost trustworthy

--- nixops/configurations/server.pascal-wittmann.de/default.nix
+++ nixops/configurations/server.pascal-wittmann.de/default.nix
@@ -137,6 +137,13 @@
     # PostgreSQL.
     services.postgresql.enable = true;
     services.postgresql.package = pkgs.postgresql92;
+    services.postgresql.authentication = lib.mkForce ''
+    # Generated file; do not edit!
+    # TYPE  DATABASE        USER            ADDRESS                 METHOD
+    local   all             all                                     trust
+    host    all             all             127.0.0.1/32            trust
+    host    all             all             ::1/128                 trust
+    '';
 
     # Caldav / Cardav
     services.radicale.enable = true;
