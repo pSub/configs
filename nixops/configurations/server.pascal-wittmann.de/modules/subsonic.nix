1a246c56d1db6e345a1e67a45aded552b7653ded
Pascal Wittmann <mail@pascal-wittmann.de>
2019-12-29 12:38:04 +0100

    [nixops/subsonic] do not force SSL
    Do not force SSL as the subsonic apps fail with "javax.net.ssl.SSLPeerUnverifiedException: No peer certificate"
    on at least Android 4.4.

--- nixops/configurations/server.pascal-wittmann.de/modules/subsonic.nix
+++ nixops/configurations/server.pascal-wittmann.de/modules/subsonic.nix
@@ -22,7 +22,10 @@ in {
   config = mkIf cfg.enable {
     services.nginx.virtualHosts = {
        "${cfg.hostname}" = {
-         forceSSL = true;
+         # Do not force SSL as the subsonic apps fail with "javax.net.ssl.SSLPeerUnverifiedException: No peer certificate"
+         # on at least Android 4.4.
+         forceSSL = false;
+         addSSL = true;
          enableACME = true;
          locations."/" = { proxyPass = "http://${subsonicHost}:${toString subsonicPort}"; };
          extraConfig = ''
