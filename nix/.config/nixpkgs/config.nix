30568e6e87bd5b6ff736e2cc2655dc5faf9e698f
Pascal Wittmann <mail@pascal-wittmann.de>
2022-03-20 22:50:41 +0100

    [nix] update to nixos-21.11 channel

--- nix/.config/nixpkgs/config.nix
+++ nix/.config/nixpkgs/config.nix
@@ -1,6 +1,6 @@
 let
   unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
-  stable = import (fetchTarball https://nixos.org/channels/nixos-21.05/nixexprs.tar.xz) { };
+  stable = import (fetchTarball https://nixos.org/channels/nixos-21.11/nixexprs.tar.xz) { };
 in
 {
 
