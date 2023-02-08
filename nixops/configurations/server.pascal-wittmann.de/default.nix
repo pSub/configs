fa1c7254d5e476af9ccc8baa1ace6453fc0b33a4
Pascal Wittmann <mail@pascal-wittmann.de>
2023-02-08 23:22:12 +0100

    [nixops] update Nextcloud to 25

--- nixops/configurations/server.pascal-wittmann.de/default.nix
+++ nixops/configurations/server.pascal-wittmann.de/default.nix
@@ -227,7 +227,7 @@
 
       # nextcloud
       services.nextcloud.enable = true;
-      services.nextcloud.package = pkgs.nextcloud24;
+      services.nextcloud.package = pkgs.nextcloud25;
       services.nextcloud.home = "/srv/nextcloud";
       services.nextcloud.config.adminpassFile = "/var/keys/nextcloud";
       services.nextcloud.hostName = "cloud.pascal-wittmann.de";
