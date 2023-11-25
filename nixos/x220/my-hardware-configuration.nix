9870893e06db4050d6babf2332dc9916cc7e10b9
Pascal Wittmann <mail@pascal-wittmann.de>
2023-11-25 22:31:21 +0100

    [nixos] grub version is not needed anymore

--- nixos/x220/my-hardware-configuration.nix
+++ nixos/x220/my-hardware-configuration.nix
@@ -30,9 +30,8 @@
 
   boot.kernelParams = [ "quiet" ];
 
-  # Use the GRUB 2 boot loader.
+  # Use the GRUB boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
 
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/disk/by-id/wwn-0x500a07510338a9bf";
