a142060831199865d5bbd9e86f4b52fe818835d3
Pascal Wittmann <pascal.wittmann@red6-es.de>
2024-03-13 14:43:03 +0100

    [nixos/server] action to unlock luks

--- nixos/server/users.nix
+++ nixos/server/users.nix
@@ -40,6 +40,7 @@
       group = "wheel";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIObCG2o/yDrsEc/uHMAL2TDxRWvSmf1zL1zT7CG15kbE deployer@pascal-wittmann.de"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiS9rptVuc7SSZRFLHRUo2Hv7lN9hHQEyCFLNP9o9HC pascal@github_deploy_server"
       ];
     };
   };
