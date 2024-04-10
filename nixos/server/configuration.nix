5fa6edfcb72b70e1668cf074f496ca73f49aa0b4
Pascal Wittmann <mail@pascal-wittmann.de>
2024-04-10 23:32:14 +0200

    [nixos/server] add reverse-proxy config for immich

--- nixos/server/configuration.nix
+++ nixos/server/configuration.nix
@@ -823,6 +823,39 @@
           };
         };
 
+        "immich.pascal-wittmann.de" = {
+          forceSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:2283";
+            extraConfig = ''
+              if ($is_allowed = 0) {
+                return 403;
+              }
+
+              # allow large file uploads
+              client_max_body_size 50000M;
+
+              # Set headers
+              proxy_set_header Host              $host;
+              proxy_set_header X-Real-IP         $remote_addr;
+              proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+
+              # enable websockets
+              proxy_http_version 1.1;
+              proxy_set_header   Upgrade    $http_upgrade;
+              proxy_set_header   Connection "upgrade";
+              proxy_redirect     off;
+
+              # set timeout
+              proxy_read_timeout 600s;
+              proxy_send_timeout 600s;
+              send_timeout       600s;
+            '';
+          };
+        };
+
         "users.pascal-wittmann.de" = {
           forceSSL = true;
           enableACME = true;
