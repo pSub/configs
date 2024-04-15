10cc7d408f18d304c7532b6cc0d29a62509dc864
Pascal Wittmann <mail@pascal-wittmann.de>
2024-04-15 22:52:31 +0200

    [doom] disable evil-mode

--- doom/init.el
+++ doom/init.el
@@ -54,7 +54,7 @@
        ;;zen               ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
+       ;;(evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
