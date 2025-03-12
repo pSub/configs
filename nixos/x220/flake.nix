db86b3925e7da0421a8bf447be31c03a82e0236d
Pascal Wittmann <mail@pascal-wittmann.de>
2025-03-12 23:42:12 +0100

    [nixos/x220] update flakes

--- nixos/x220/flake.nix
+++ nixos/x220/flake.nix
@@ -2,16 +2,17 @@
   description = "NixOS configuration";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
+  inputs.unstable.url = "nixpkgs/nixos-unstable";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   inputs.sops-nix.url = "github:Mic92/sops-nix";
   inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = inputs @ { nixpkgs, nixos-hardware, sops-nix, ... }:
+  outputs = inputs @ { nixpkgs, unstable, nixos-hardware, sops-nix, ... }:
     let system = "x86_64-linux"; in {
       nixosConfigurations = {
         x220 = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit inputs; };
+          specialArgs = { unstable = import unstable { inherit system; }; };
           modules = [
             nixos-hardware.nixosModules.lenovo-thinkpad-x220
             ./my-hardware-configuration.nix
