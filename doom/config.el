838bda94703d6ed49962c4ec0f1b084c0da106c3
Pascal Wittmann <mail@pascal-wittmann.de>
2025-03-12 23:41:22 +0100

    [doom] fix wakapi installation

--- doom/config.el
+++ doom/config.el
@@ -74,7 +74,3 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(use-package wakatime-mode
-  :ensure t)
-
-(global-wakatime-mode)
---
+++ doom/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(wakatime-mode nix-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(global-wakatime-mode)
