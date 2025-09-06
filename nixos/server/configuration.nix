b6b9ff54a8b775f1cc2e121d805a7fc648b2fa91
Pascal Wittmann <pascal.wittmann@red6-es.de>
2025-09-06 23:43:31 +0200

    [nixos/server/koillection] fix invalid csrf token error

--- nixos/server/configuration.nix
+++ nixos/server/configuration.nix
@@ -1087,6 +1087,14 @@ in  {
           enableACME = true;
           locations."/" = {
             proxyPass = "http://127.0.0.1:3046";
+            extraConfig = ''
+              # Set headers
+              proxy_set_header Host              $host;
+              proxy_set_header X-Real-IP         $remote_addr;
+              proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_set_header X-Forwarded-Server $host;
+              '';
           };
         };
 
