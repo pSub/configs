fb9f68ceca48fcb77e61a9bbb6802c46dae95933
Pascal Wittmann <pascal.wittmann@red6-es.de>
2025-09-19 10:50:06 +0200

    [nixos/server] wakapi cannot be geo-restricted as it is access through github

--- nixos/server/configuration.nix
+++ nixos/server/configuration.nix
@@ -958,11 +958,6 @@ in  {
           enablePhare = true;
           locations."/" = {
             proxyPass = "http://127.0.0.1:3043";
-            extraConfig = ''
-              if ($is_allowed = 0) {
-                return 403;
-              }
-            '';
           };
         };
 
