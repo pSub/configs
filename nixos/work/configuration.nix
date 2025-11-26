fe50240d1fd23494cf338daf32d1a7a854a930a7
Pascal Wittmann <pascal.wittmann@red6-es.de>
2025-11-26 13:32:41 +0100

    [nixos/work] install direnv

--- nixos/work/configuration.nix
+++ nixos/work/configuration.nix
@@ -163,8 +163,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    direnv
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
